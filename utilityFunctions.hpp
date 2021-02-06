59f3543862a4848dd454bb726af6ddc5390995af
skyawmyint <44180667+skyawmyint@users.noreply.github.com>
2021-02-06 12:53:55 -0800

    removed temporary coding in utilityFunctions

--- utilityFunctions.hpp
+++ utilityFunctions.hpp
@@ -10,12 +10,6 @@
 
 using std::string;
 
-int checkIntMinMax(string testIntIn, int MaxIn, int MinIn = 1);
-int checkSelection2(string selectIn);
-int checkSelection3(string selectIn);
-int checkSelection3a(string selectIn);
-int checkSelection5(string selectIn);
-
 
 #endif  
 
